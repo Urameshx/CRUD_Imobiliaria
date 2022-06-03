2d0bbd3ffa896a5c8b4f462663d16e5c3bb37482
Lucas Henrique <88640471+Urameshx@users.noreply.github.com>
2022-06-03 10:12:22 -0300

    Update query.sql

--- CRUDImv/sql/query.sql
+++ CRUDImv/sql/query.sql
@@ -5,5 +5,15 @@ USE imob;
 CREATE TABLE Imovel (
   codigo int NOT NULL,
   endereco  varchar(100) NOT NULL,
+  bairo varchar(100) NOT NULL,
+  cep char(8) NOT NULL
   PRIMARY KEY (codigo)
+);
+
+CREATE TABLE corretor (
+  creci CHAR(7) NOT NULL,
+  nome  varchar(100) NOT NULL,
+  regiao VARCHAR(200) NOT NULL,
+  telefone CHAR(15),
+  PRIMARY KEY (creci)
 );
